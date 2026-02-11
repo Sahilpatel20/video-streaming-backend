8a137450bfe3402940a952429eef71d7c5af8ef4
Sahilpatel20 <sahilh.patel36@gmail.com>
2026-02-11 15:40:39 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -12,6 +12,12 @@ app.use(cors({
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
+app.use(cookieparser())
 
 
+import user from "./routes/user.js"
+
+app.use("/users",user)
+
+//it will work like this => http://localhost:3000/users/register => then func call registeruser
 export {app}
